2520add18dbddd0c208af2426a8c04d4a7bc682a
doddy <rebecca.dodd@gmail.com>
2017-10-12 01:32:36 -0500

    Changed layout to match example in assignment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -10,8 +10,8 @@ db = SQLAlchemy(app)
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    body = db.Column(db.String(1000))
+    title = db.Column(db.String(140))
+    body = db.Column(db.String(2000))
 
     def __init__(self, title, body):
         self.title = title
@@ -22,13 +22,11 @@ def index():
     if request.args:
         blog_id = request.args.get("id")
         blog = Blog.query.get(blog_id)
-
-        return render_template('blogentry.html', blog=blog)
+        return render_template('blogpost.html', blog=blog)
 
     else:
-        blogs = Blog.query.all()
-        
-        return render_template('blog.html', title="Build a Blog", blogs=blogs)
+        all_blogs = Blog.query.all()
+        return render_template('blog.html', title="Build a Blog", all_blogs=all_blogs)
 
 @app.route('/newpost', methods=['GET', 'POST'])
 def add_blog():
@@ -38,26 +36,14 @@ def add_blog():
     if request.method == 'POST':
         blog_title = request.form['title']
         blog_body = request.form['body']
-        title_error = ""
-        body_error = ""
-
-    if len(blog_title) < 1:
-        title_error = "Please add a valid title"
-
-    if len(blog_body) < 1:
-        body_error = "Please add a valid post"
-
-    if not title_error and not body_error:
-        new_blog = Blog(blog_title, blog_body)
-        db.session.add(new_blog)
+        new_blog_post = Blog(blog_title, blog_body)
+        db.session.add(new_blog_post)
         db.session.commit()
-        query_param_url ="/blog?id=" + str(new_blog.id)
-        return redirect(query_param_url)
+        blog_entry ="/blog?id=" + str(new_blog_post.id)
+        return redirect(blog_entry)
     
     else:
-        return render_template('newpost.html', title="Add Blog Entry"
-                                             , title_error=title_error
-                                             , body_error=body_error)
+        return render_template('newpost.html', title="Add Blog Entry")
 
 if __name__ == '__main__':
     app.run()
